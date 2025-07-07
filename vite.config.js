aec46355d2eba385df65436bab59650390ed046f
daradanilova <lemma1102@gmail.com>
2025-07-07 17:06:46 +0300

    Update styles and migrate SCSS @import to @use

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/altanschool-site/',
+	// base: '/altanschool-site/',
   plugins: [react()],
 })
